3c7251c5aacee60da006e9b0c666f2cb98b1cae5
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2025-11-17 11:37:08 +0600

    adding java script part

---
+++ labtask_2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body{
+                    font-family: 'Times New Roman', Times, serif;
+                    padding: 10px;
+                    background-color: white;
+                    color: cyan;
+                    
+                }
+            h2,h1{
+                text-align: center;
+            }
+            .section-box{
+                background-color:white;
+                padding: 15px;
+                width: 200px;
+                margin: auto;
+                border-radius: 10px;
+            }
+            input,button{
+                width: 100%;
+                padding:5px;
+                margin: auto;
+
+            }
+            button{
+
+            }
+            </body>
+
+    </head>
+</html>
---
+++ rab.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html >
+<head>
+  <h2>Student Registration Information</h2>
+<style>
+  .course-selection{
+    border-bottom:4px solid red;
+    color:red;
+  }
+</style>
+</head>
+<body>
+<h4>First name</h4>
+<input type="text">
+<h4>Last name</h4>
+<input type="text">
+<h4>Student Id</h4>
+<input type="text">
+<h4> Selct your Programe</h4>
+<select>
+<option value="Bsc in CSE">CSE</option>
+<option value="Bsc in EEE">EEE</option>
+<option value="Bsc in ETE">ETE</option>
+</select>
+<h4> Selct your Department</h4>
+<select>
+<option value="FASS">FASS</option>
+<option value="FST">FST</option>
+<option value="FE">FE</option>
+</select>
+<h4>University Email</h4>
+<input type="text">
+<h4>Create password</h4>
+<input type="text">
+<h4>Confirm password</h4>
+<input type="text">
+<h4>Semester Selection</h4>
+<input type="radio" name="SE" value "1st">Fall 24-25
+<input type="radio" name="SE" value "2nd">Summer 24-25
+<h4>Request Credit</h4>
+<input type="number">
+
+<p class="course-selection">Course Selection</p>
+<input type="checkbox">Web_tech
+<input type="checkbox">Math
+<input type="checkbox">English
+<input type="checkbox">physics
+<input type="checkbox">Bangla
+<h3>Comment</h3>
+<input type="text"><br>
+<center><input type="Submit" value="Submit"></center>
+</body>
+</html>
